1c9ab6ba09cca9bf8af0ef75971ac96f328d0ed0
Mario23junior <mariojunior3251@gmail.com>
2022-04-02 05:12:41 -0300

    extendendo JPA

--- src/main/java/com/project/todoInfom/repository/UserRepository.java
+++ src/main/java/com/project/todoInfom/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.project.todoInfom.repository;
 
-public interface UserRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.todoInfom.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
