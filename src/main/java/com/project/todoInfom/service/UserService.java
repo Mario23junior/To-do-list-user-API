ae5d379560f9d15aff8d187ec61e27c63829555a
Mario23junior <mariojunior3251@gmail.com>
2022-04-03 01:55:46 -0300

    Criando camada de listando de informações por id

---
+++ src/main/java/com/project/todoInfom/service/UserService.java
@@ -0,0 +1,8 @@
+package com.project.todoInfom.service;
+
+import com.project.todoInfom.model.User;
+
+public interface UserService {
+  
+	User findById(long id);
+}
---
+++ src/main/java/com/project/todoInfom/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.project.todoInfom.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.todoInfom.model.User;
+import com.project.todoInfom.repository.UserRepository;
+import com.project.todoInfom.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+  
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public User findById(long id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElse(null);
+ 	}
+
+}
