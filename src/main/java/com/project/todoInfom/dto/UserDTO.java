88744b926bfa9e18bfb7ef461e379bf2a8cfaec5
Mario23junior <mariojunior3251@gmail.com>
2022-04-03 20:31:11 -0300

    Inserindo dependencia model mapper

---
+++ src/main/java/com/project/todoInfom/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.project.todoInfom.dto;
+
+public class UserDTO {
+  
+	private Long id;
+	private String nome;	
+ 	private String email;
+	private String password;
+	
+	public UserDTO() {
+ 	}
+
+	public UserDTO(Long id, String nome, String email, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
