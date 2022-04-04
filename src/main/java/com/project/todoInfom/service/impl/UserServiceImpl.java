cf8222cd2339ce5017812cc2e82954f7ceecad54
Mario23junior <mariojunior3251@gmail.com>
2022-04-04 00:13:48 -0300

    Salvando usuarios no banco de dados

--- src/main/java/com/project/todoInfom/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/todoInfom/service/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.project.todoInfom.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.todoInfom.dto.UserDTO;
 import com.project.todoInfom.exceptions.ObjectNotFoundExecution;
 import com.project.todoInfom.model.User;
 import com.project.todoInfom.repository.UserRepository;
@@ -17,6 +19,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
@@ -27,5 +32,10 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<User> ListAllBase() {
  		return repository.findAll();
+	}
+
+	@Override
+	public User creaet(UserDTO obj) {
+ 		return repository.save(mapper.map(obj, User.class));
 	}
 }
