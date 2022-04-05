cb0cda1abc0f356fec9c3ed7052afc2b38a63d47
Mario23junior <mariojunior3251@gmail.com>
2022-04-05 03:50:15 -0300

    Criando estrutura para testes de interface

---
+++ src/test/java/com/project/todoInfom/service/impl/UserServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.project.todoInfom.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceImplTest {
+
+	private UserServiceImpl service;
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void findById() {
+
+	}
+
+	@Test
+	void findAll() {
+
+	}
+
+	@Test
+	void create() {
+
+	}
+
+	@Test
+	void update() {
+
+	}
+
+	@Test
+	void delete() {
+
+	}
+
+}
