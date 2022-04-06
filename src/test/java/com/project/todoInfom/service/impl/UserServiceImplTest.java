73f7e9bec32381e8090c3d41e593ff90420e799c
Mario23junior <mariojunior3251@gmail.com>
2022-04-06 04:19:26 -0300

    Criando teste unitario da camada de listagem por ID

--- src/test/java/com/project/todoInfom/service/impl/UserServiceImplTest.java
+++ src/test/java/com/project/todoInfom/service/impl/UserServiceImplTest.java
@@ -1,19 +1,64 @@
 package com.project.todoInfom.service.impl;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.todoInfom.dto.UserDTO;
+import com.project.todoInfom.model.User;
+import com.project.todoInfom.repository.UserRepository;
+
+@SpringBootTest
 public class UserServiceImplTest {
 
+	@InjectMocks
 	private UserServiceImpl service;
+
+	@Mock
+	private UserRepository repository;
+
+	@Mock
+	private ModelMapper mapper;
+
+	private User user;
+	private UserDTO userDto;
+	private Optional<User> optionalUser;
+
+	Long ID = 1L;
+	String NAME = "Orochimaro";
+	String EMAIL = "tioOrochimaru@gmai.com";
+	String PASSWORD = "123";
 
 	@BeforeEach
 	void setUp() {
-
+		MockitoAnnotations.openMocks(this);
+		startUser();
 	}
 
 	@Test
-	void findById() {
+	@DisplayName("Teste de listagem de usuario por id")
+	void whenFindByIdThenReturnUserInstance() {
+		Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(optionalUser);
+		User response = service.findById(ID);
+		
+		assertNotNull(response);
+		
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getNome());
+		assertEquals(EMAIL, response.getEmail());
 
 	}
 
@@ -34,6 +79,13 @@ public class UserServiceImplTest {
 
 	@Test
 	void delete() {
+
+	}
+
+	private void startUser() {
+		user = new User(ID, NAME, EMAIL, PASSWORD);
+		userDto = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+		optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
 
 	}
 
