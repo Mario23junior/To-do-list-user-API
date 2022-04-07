1ebe4a705444d70cc21efe07e78f721ec8d414bc
Mario23junior <mariojunior3251@gmail.com>
2022-04-07 03:15:21 -0300

    Teste paraa classe de ciracao de usuario retornando exception

--- src/test/java/com/project/todoInfom/service/impl/UserServiceImplTest.java
+++ src/test/java/com/project/todoInfom/service/impl/UserServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.project.todoInfom.dto.UserDTO;
+import com.project.todoInfom.exceptions.IntegrityViolationOnlyException;
 import com.project.todoInfom.exceptions.ObjectNotFoundExecution;
 import com.project.todoInfom.model.User;
 import com.project.todoInfom.repository.UserRepository;
@@ -43,8 +45,7 @@ public class UserServiceImplTest {
 	String NAME = "Orochimaro";
 	String EMAIL = "tioOrochimaru@gmai.com";
 	String PASSWORD = "123";
-	String ErrorReturnMesaage = "O id "+ID+" não foi encontrado pois não "
-			+ "existe cadastro associado a este ID";
+	String ErrorReturnMesaage = "O id " + ID + " não foi encontrado pois não " + "existe cadastro associado a este ID";
 
 	@BeforeEach
 	void setUp() {
@@ -57,67 +58,78 @@ public class UserServiceImplTest {
 	void whenFindByIdThenReturnUserInstance() {
 		Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(optionalUser);
 		User response = service.findById(ID);
-		
+
 		assertNotNull(response);
-		
+
 		assertEquals(User.class, response.getClass());
 		assertEquals(ID, response.getId());
 		assertEquals(NAME, response.getNome());
 		assertEquals(EMAIL, response.getEmail());
 
 	}
-	
-	
+
 	@Test
 	@DisplayName("Teste de falha de listagem de usuario por ID ")
 	void WhenFindByIdThenReturnAnObjectNotFundException() {
-		Mockito.when(repository.findById(anyLong())).thenThrow(new ObjectNotFoundExecution(ErrorReturnMesaage));	
-		
+		Mockito.when(repository.findById(anyLong())).thenThrow(new ObjectNotFoundExecution(ErrorReturnMesaage));
+
 		try {
 			service.findById(ID);
 		} catch (Exception ex) {
 			assertEquals(ObjectNotFoundExecution.class, ex.getClass());
 			assertEquals(ErrorReturnMesaage, ex.getMessage());
- 		}
-		
+		}
+
 	}
 
 	@Test
 	@DisplayName("Testando listagem de todos os usuarios")
 	void findAll() {
-       Mockito.when(repository.findAll()).thenReturn(List.of(user));
-       
-       List<User> response = service.ListAllBase();
-       
-       assertNotNull(response);
-       assertEquals(1L, response.size());
-       int INDEXInput = 0;
-	assertEquals(User.class, response.get(INDEXInput).getClass());
-       
-       assertEquals(ID, response.get(INDEXInput).getId());
-       assertEquals(NAME, response.get(INDEXInput).getNome());
-       assertEquals(EMAIL, response.get(INDEXInput).getEmail());
-       assertEquals(PASSWORD, response.get(INDEXInput).getPassword());
+		Mockito.when(repository.findAll()).thenReturn(List.of(user));
 
+		List<User> response = service.ListAllBase();
+
+		assertNotNull(response);
+		assertEquals(1L, response.size());
+		int INDEXInput = 0;
+		assertEquals(User.class, response.get(INDEXInput).getClass());
+
+		assertEquals(ID, response.get(INDEXInput).getId());
+		assertEquals(NAME, response.get(INDEXInput).getNome());
+		assertEquals(EMAIL, response.get(INDEXInput).getEmail());
+		assertEquals(PASSWORD, response.get(INDEXInput).getPassword());
 
 	}
 
 	@Test
 	@DisplayName("Ao criar um novo usuario retornar sucesso")
-	void whenCreateTheReturnSuccess() {  
-       Mockito.when(repository.save(any())).thenReturn(user);
-       
-       User response = service.creaet(userDto);
-       
-       assertNotNull(response);
-       assertEquals(User.class, response.getClass());
-       assertEquals(ID, response.getId());
-       assertEquals(EMAIL, response.getEmail());
-       assertEquals(NAME, response.getNome());
-       assertEquals(PASSWORD, response.getPassword());
+	void whenCreateTheReturnSuccess() {
+		Mockito.when(repository.save(any())).thenReturn(user);
+
+		User response = service.creaet(userDto);
+
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(NAME, response.getNome());
+		assertEquals(PASSWORD, response.getPassword());
 	}
-	
-	
+
+	@Test
+	@DisplayName("Teste de violação de emails repetidos retornar exceptions")
+	void whenCreateTheReturnAndVioletionData() {
+		Mockito.when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+		try {
+			optionalUser.get().setId(2L);
+			service.creaet(userDto);
+		} catch (Exception ex) {
+			assertEquals(IntegrityViolationOnlyException.class, ex.getClass());
+			assertEquals("O E-mail "+userDto.getEmail()+" Já existe um cadastro com este e-mail", ex.getMessage());
+		}
+
+	}
 
 	@Test
 	void update() {
